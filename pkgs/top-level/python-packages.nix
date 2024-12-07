7420527323eba62ac02d80521a917581cb3df12b
Peter Kling <1018801+pitkling@users.noreply.github.com>
2024-12-07 15:47:55 +0100

    patch from branch: gpio-packages

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7294,6 +7294,12 @@ self: super: with self; {
 
   lexilang = callPackage ../development/python-modules/lexilang { };
 
+  lgpio = toPythonModule (pkgs.lgpio.override {
+    inherit buildPythonPackage;
+    pyProject = "PY_LGPIO";
+    lgpioWithoutPython = pkgs.lgpio;
+  });
+
   lhapdf = toPythonModule (pkgs.lhapdf.override { inherit python; });
 
   lib4sbom = callPackage ../development/python-modules/lib4sbom { };
@@ -10445,6 +10451,10 @@ self: super: with self; {
   piep = callPackage ../development/python-modules/piep { };
 
   piexif = callPackage ../development/python-modules/piexif { };
+
+  pigpio = toPythonModule (pkgs.pigpio.override {
+    inherit buildPythonPackage;
+  });
 
   pijuice = callPackage ../development/python-modules/pijuice { };
 
@@ -14267,6 +14277,11 @@ self: super: with self; {
   rfcat = callPackage ../development/python-modules/rfcat { };
 
   rflink = callPackage ../development/python-modules/rflink { };
+
+  rgpio = toPythonModule (pkgs.lgpio.override {
+    inherit buildPythonPackage;
+    pyProject = "PY_RGPIO";
+  });
 
   rich = callPackage ../development/python-modules/rich { };
 
